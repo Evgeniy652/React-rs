acf81bdb5962690d76554422f33d8d1c79af92fb
Evgeniy652 <evgen652@mail.ru>
2022-09-26 23:20:31 +0300

    feat:add search sorting cards and functionality localstorage

--- react-app/src/components/pages/home/cards.tsx
+++ react-app/src/components/pages/home/cards.tsx
@@ -5,14 +5,27 @@ import data from './data.json';
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 class Cards extends React.Component<any, any> {
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  dataStandart: any;
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
   constructor(props: any) {
     super(props);
-    this.state = { data };
+    const dataArr = data.results;
+    this.state = { dataArr };
+    this.dataStandart = data.results;
+  }
+
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  sortingCards(): any[] {
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars, @typescript-eslint/no-explicit-any
+    const filterCards = this.dataStandart.filter((card: any) => {
+      return card.name.toLowerCase().includes(this.props.value.toLowerCase());
+    });
+    return filterCards;
   }
 
   createCards() {
     // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    const arrCards = this.state.data.results.map((el: any) => {
+    const arrCards = this.state.dataArr.map((el: any) => {
       return (
         <div className="card" key={el.id}>
           <div className="card-img">
@@ -36,10 +49,20 @@ class Cards extends React.Component<any, any> {
     return arrCards;
   }
 
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  componentDidUpdate(prevProps: Readonly<any>): void {
+    if (this.props.value != prevProps.value) {
+      const dataArr = this.sortingCards();
+      this.setState({ dataArr });
+      return;
+    }
+  }
+
   render(): React.ReactNode {
     const res = this.createCards();
-    return <div className="cards">{res}</div>;
+    const content =
+      res.length > 0 ? res : <p className="text-none">No matches were found for your search</p>;
+    return <div className="cards">{content}</div>;
   }
 }
-
 export default Cards;
--- react-app/src/components/pages/home/home.tsx
+++ react-app/src/components/pages/home/home.tsx
@@ -1,11 +1,16 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import React from 'react';
 import Cards from './cards';
 import './home.css';
 
-class Home extends React.Component {
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+class Home extends React.Component<any, any> {
+  actualValue: any;
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
   constructor(props: any) {
     super(props);
+    this.state = { value: '' };
+    this.actualValue = localStorage.getItem('value');
   }
   render(): React.ReactNode {
     return (
@@ -16,16 +21,28 @@ class Home extends React.Component {
             id="input__search"
             name="search"
             autoComplete="off"
+            value={this.actualValue}
             placeholder="Search by name"
-          ></input>
+            onChange={(event) => {
+              const obj = { value: event.target.value };
+              this.actualValue = event.target.value;
+              this.setState(obj);
+            }}
+          />
         </div>
-        <Cards />
+        <Cards value={this.state.value} />
       </div>
     );
   }
 
-  componentDidMount(): void {}
-  componentWillUnmount(): void {}
+  componentDidMount(): void {
+    if (this.actualValue) {
+      this.setState({ value: this.actualValue });
+    }
+  }
+  componentWillUnmount(): void {
+    localStorage.setItem('value', this.actualValue);
+  }
 }
 
 export default Home;
